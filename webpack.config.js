67df43a009ab07adc5d92fd807f1d6da956b778a
wt <307548594@qq.com>
2018-07-16 13:20:39 +0800

    webpack对html 字体 文件的处理

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@
  * @Author: wt 
  * @Date: 2018-07-16 10:35:33 
  * @Last Modified by: wt
- * @Last Modified time: 2018-07-16 11:58:40
+ * @Last Modified time: 2018-07-16 12:52:43
  */
 var webpack = require('webpack');// 22行有个webpack
 var extractTextPlugin = require('extract-text-webpack-plugin');//提取css单独打包
+var HtmlWebpackPlugin = require('html-webpack-plugin');//处理HTML
+
+// 获取html-webpack-plugin参数
+var getHtmlConfig = function(name) {
+    return {
+        template: './src/view/'+name+'.html', // 源模板文件
+        filename: 'view/'+name+'.html', // 输出文件
+        inject: true, // 所有的 javascript 资源将被放置到 body 元素的底部
+        hash: true, // 生成hash添加在引入文件地址的末尾
+        chunks: ['common',name] // entry中设置多个js时，在这里指定引入的js，如果不设置则默认全部引入
+    }
+}
 
 var config = {
     entry: {
@@ -23,7 +35,8 @@ var config = {
     module: {
         loaders: [
               // {test:/\.css$/,loader:'style-loader!css-loader'}, // 将css打包到js中了
-            {test: /\.css$/, loader: extractTextPlugin.extract("style-loader","css-loader")}
+            {test: /\.css$/, loader: extractTextPlugin.extract("style-loader","css-loader")},
+            { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
         ]
     },
     plugins: [
@@ -32,6 +45,9 @@ var config = {
             filename: 'js/base.js',// 基于path: './dist'底下的目录
         }),
         new extractTextPlugin("css/[name].css"),// css单独打包
+        // html模板的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
 }
 
