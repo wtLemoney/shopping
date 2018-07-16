d76b993477ffe2a0a3410949a2a840e455de83a9
wt <307548594@qq.com>
2018-07-16 14:06:34 +0800

    webpack-dev-server配置

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,11 +2,10 @@
  * @Author: wt 
  * @Date: 2018-07-16 10:42:25 
  * @Last Modified by: wt
- * @Last Modified time: 2018-07-16 11:25:14
+ * @Last Modified time: 2018-07-16 13:53:32
  */
 'use strict';
-var $$=require('jquery');
+//var $$=require('jquery');
 require('./index.css');
 
 console.log('hello index');
-$$('body').html('index hahsadffhh')
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@
  * @Author: wt 
  * @Date: 2018-07-16 10:35:33 
  * @Last Modified by: wt
- * @Last Modified time: 2018-07-16 12:52:43
+ * @Last Modified time: 2018-07-16 14:00:38
  */
 var webpack = require('webpack');// 22行有个webpack
 var extractTextPlugin = require('extract-text-webpack-plugin');//提取css单独打包
 var HtmlWebpackPlugin = require('html-webpack-plugin');//处理HTML
+
+// 环境变量配置，dev / online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
 
 // 获取html-webpack-plugin参数
 var getHtmlConfig = function(name) {
@@ -19,14 +22,16 @@ var getHtmlConfig = function(name) {
     }
 }
 
+
 var config = {
     entry: {
-        'common' : ['./src/page/common/index.js'],
+        'common' : ['./src/page/common/index.js','webpack-dev-server/client?http://localhost:8088/'],
         'index' : ['./src/page/index/index.js'],
         'login' : ['./src/page/login/index.js']
     },
     output: {
         path: './dist',
+        publicPath : '/dist',
         filename: 'js/[name].js'
     },
     externals: {// 外部依赖的声明
@@ -35,7 +40,7 @@ var config = {
     module: {
         loaders: [
               // {test:/\.css$/,loader:'style-loader!css-loader'}, // 将css打包到js中了
-            {test: /\.css$/, loader: extractTextPlugin.extract("style-loader","css-loader")},
+            { test: /\.css$/, loader: extractTextPlugin.extract("style-loader","css-loader") },
             { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
         ]
     },
@@ -49,6 +54,11 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('index')),
         new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
+}
+
+
+if('dev' === WEBPACK_ENV){
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
 }
 
 module.exports = config;
